48653a389baed9b4959a8e8a43ae93fd693a3a95
Trevor Murphy <trevor.murphy@avenuecode.com>
2022-07-14 15:28:14 -0500

    Verify frontend to backend connection is working

--- web/index.js
+++ web/index.js
@@ -11,5 +11,17 @@ window.addEventListener('keypress', (event) => {
 
 button.addEventListener('click', () => {
   const url = input.value;
-  document.getElementById('shortened').textContent = url;
+  fetch('http://localhost:8080/urls', {
+    method: 'POST',
+    body: JSON.stringify({ shortUrl: 'something', originalUrl: url }),
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then((res) => res.json())
+    .then(
+      (json) => {
+        console.log(json);
+        document.getElementById('shortened').textContent = url;
+      },
+      (err) => console.log(err)
+    );
 });
