33b264b418c6374f700861d3a39ade94ff87f586
Trevor Murphy <trevor.murphy@avenuecode.com>
2022-07-14 15:05:34 -0500

    Allow hitting "Enter" to click the button

--- web/index.js
+++ web/index.js
@@ -1,6 +1,14 @@
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 
+// Window event listener for handling "Enter" key, which will click the button
+window.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    button.click();
+  }
+});
+
 button.addEventListener('click', () => {
   const url = input.value;
   document.getElementById('shortened').textContent = url;
