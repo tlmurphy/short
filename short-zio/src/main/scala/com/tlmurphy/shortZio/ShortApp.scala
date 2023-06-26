06c6251c70707889cabe42487f2862d59916e65b
Trevor Murphy <trevorlm95@gmail.com>
2023-06-25 21:29:05 -0500

    Implement ShortApp with all routes

--- short-zio/src/main/scala/com/tlmurphy/shortZio/ShortApp.scala
+++ short-zio/src/main/scala/com/tlmurphy/shortZio/ShortApp.scala
@@ -1,10 +1,64 @@
 package com.tlmurphy.shortZio
 
 import zhttp.http.*
+import zio.*
+import zio.json.*
+import models.*
 
-object ShortApp {
-  def apply(): Http[UrlRepo, Nothing, Request, Response] =
-    Http.collect[Request] { case Method.GET -> !! / "hello" =>
-      Response.text("Hello there!")
+object ShortApp:
+  def apply(): Http[UrlRepo, Throwable, Request, Response] =
+    Http.collectZIO[Request] {
+      case Method.GET -> !! / "urls" =>
+        UrlRepo.getAll
+          .map(repo => Response.json(GetAllResponse(repo.values.toList).toJson))
+
+      case Method.GET -> !! / "urls" / url =>
+        UrlRepo
+          .get(url)
+          .map {
+            case Some(u) =>
+              Response.json(
+                GetResponse(s"$u successfully retrieved", u).toJson
+              )
+            case None => Response.status(Status.NotFound)
+          }
+
+      case req @ Method.POST -> !! / "urls" =>
+        for {
+          body <- req.body.asString.map(_.fromJson[PostBody])
+          req <- body match {
+            case Left(e) =>
+              ZIO
+                .debug(s"Failed to parse the input: $e")
+                .as(Response.text(e).setStatus(Status.BadRequest))
+            case Right(b) =>
+              for {
+                short <- ShortUrlGenerator.generateShortUrl
+                shortUrl = ShortUrl(short, b.url)
+                addResult <- UrlRepo.add(shortUrl)
+              } yield Response
+                .json(
+                  CreateResponse(
+                    s"$shortUrl successfully created",
+                    shortUrl
+                  ).toJson
+                )
+                .setStatus(Status.Created)
+          }
+        } yield req
+
+      case Method.DELETE -> !! / "urls" / url =>
+        UrlRepo
+          .remove(url)
+          .map(_ =>
+            Response.json(
+              DeleteResponse(s"Short URL $url successfully deleted.").toJson
+            )
+          )
+
+      case Method.GET -> !! / shortUrl =>
+        UrlRepo.get(shortUrl).map {
+          case Some(u) => Response.redirect(u.originalUrl, true)
+          case None    => Response.status(Status.NotFound)
+        }
     }
-}
--- short-zio/src/main/scala/com/tlmurphy/shortZio/models/Http.scala
+++ short-zio/src/main/scala/com/tlmurphy/shortZio/models/Http.scala
@@ -3,26 +3,26 @@ package com.tlmurphy.shortZio.models
 import zio.json.*
 
 case class GetAllResponse(urls: List[ShortUrl])
-case class GetResponse(message: String, url: Option[ShortUrl] = None)
+case class GetResponse(message: String, url: ShortUrl)
 case class CreateResponse(message: String, url: ShortUrl)
 case class DeleteResponse(message: String)
 case class PostBody(url: String)
 
 object GetAllResponse:
-  given JsonDecoder[GetAllResponse] =
-    DeriveJsonDecoder.gen[GetAllResponse]
+  given JsonEncoder[GetAllResponse] =
+    DeriveJsonEncoder.gen[GetAllResponse]
 
 object GetResponse:
-  given JsonDecoder[GetResponse] =
-    DeriveJsonDecoder.gen[GetResponse]
+  given JsonEncoder[GetResponse] =
+    DeriveJsonEncoder.gen[GetResponse]
 
 object CreateResponse:
-  given JsonDecoder[CreateResponse] =
-    DeriveJsonDecoder.gen[CreateResponse]
+  given JsonEncoder[CreateResponse] =
+    DeriveJsonEncoder.gen[CreateResponse]
 
 object DeleteResponse:
-  given JsonDecoder[DeleteResponse] =
-    DeriveJsonDecoder.gen[DeleteResponse]
+  given JsonEncoder[DeleteResponse] =
+    DeriveJsonEncoder.gen[DeleteResponse]
 
 object PostBody:
   given JsonDecoder[PostBody] =
